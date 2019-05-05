35a89fadf0db466e88bd3ff74ba0cbc09834ffe9
Yhomarth <yhomarth@gmail.com>
2019-05-05 18:59:14 -0400

    Primer commit -colas en produccion

---
+++ public/js/socket.escritorio.js
@@ -0,0 +1,34 @@
+// Comando para establecer la comunicacion con el servidor
+
+var socket = io();
+
+var searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es necesario');
+}
+
+var escritorio = searchParams.get('escritorio');
+
+$('h1').text('Escritorio ' + escritorio);
+
+$('button').on('click', function() {
+    socket.emit('atenderTicket', { escritorio: escritorio }, function(resp) {
+
+        if (resp.message === 'No hay tickets!') {
+            $('h4').text(resp.message);
+            return;
+        }
+
+        $('h4').text('Ticket : ' + resp.numero);
+    });
+});
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Desconectado del servidor');
+});
